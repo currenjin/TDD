5e81c873915a239c0f55af896b432b532071ca37
nowjin <hyun0524e@naver.com>
2021-08-25 23:46:18 +0900

    part-1/chapter-2

--- test_driven_development_by_example/src/test/java/com/tdd/test/money/MoneyTests.java
+++ test_driven_development_by_example/src/test/java/com/tdd/test/money/MoneyTests.java
@@ -9,7 +9,9 @@ public class MoneyTests {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
-        assertEquals(10, five.amount);
+        Dollar product = five.times(2);
+        assertEquals(10, product.amount);
+        product = five.times(3);
+        assertEquals(15, product.amount);
     }
 }
--- test_driven_development_by_example/todo.md
+++ test_driven_development_by_example/todo.md
@@ -28,7 +28,7 @@
 - [ ] $5 + 10CHF = $10(환율이 2:1일 경우)
 - [x] $5 x 2 = $10
 - [ ] amount를 private으로 만들기
-- [ ] Dollar 부작용(side effect)?
+- [x] Dollar 부작용(side effect)?
 - [ ] Money 반올림?
 
 ### Acceptance criteria

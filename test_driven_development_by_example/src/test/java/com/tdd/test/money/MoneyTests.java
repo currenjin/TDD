371187e7b0eae606b339a8a002dbca9839841332
nowjin <hyun0524e@naver.com>
2021-08-26 00:33:04 +0900

    part-1/chapter-4

--- test_driven_development_by_example/src/test/java/com/tdd/test/money/MoneyTests.java
+++ test_driven_development_by_example/src/test/java/com/tdd/test/money/MoneyTests.java
@@ -9,10 +9,8 @@ public class MoneyTests {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test

08eb07e1fbf268b42796c677a16b9b06de26f41b
Hyunjin Jeong <hyun0524e@naver.com>
2021-09-02 01:46:06 +0900

    part-1/chapter-9 (#16)

--- test_driven_development_by_example/src/main/java/com/tdd/test/money/Money.java
+++ test_driven_development_by_example/src/main/java/com/tdd/test/money/Money.java
@@ -2,13 +2,23 @@ package com.tdd.test.money;
 
 abstract class Money {
     protected int amount;
+    protected String currency;
+
+    Money(int amount, String currency) {
+        this.amount = amount;
+        this.currency = currency;
+    }
+
+    String currency() {
+        return currency;
+    };
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     public boolean equals(Object object) {

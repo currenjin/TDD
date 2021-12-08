fdfab85ffebd8293797f13b2bcac45649ed3ef45
currenjin <hyun0524e@naver.com>
2021-12-09 01:08:12 +0900

    chapter-2, chapter-3

--- challenge/211209/src/test/java/com/challenge/tdd/MoneyTest.java
+++ challenge/211209/src/test/java/com/challenge/tdd/MoneyTest.java
@@ -8,8 +8,17 @@ class MoneyTest {
     @Test
     void testMultiplication() {
         Dollar five = new Dollar(5);
-        five.times(2);
+        Dollar product = five.times(2);
 
-        assertThat(five.amount).isEqualTo(10);
+        assertThat(product.amount).isEqualTo(10);
+
+        product = five.times(3);
+        assertThat(product.amount).isEqualTo(15);
+    }
+
+    @Test
+    void testEquality() {
+        assertThat(new Dollar(5).equals(new Dollar(5))).isTrue();
+        assertThat(new Dollar(5).equals(new Dollar(6))).isFalse();
     }
 }

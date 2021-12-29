3b5c7297ecc52107f6d380f0f202aaf49936071d
currenjin <hyun0524e@naver.com>
2021-12-29 23:52:19 +0900

    chapter-11

--- challenge/211209/src/main/java/com/challenge/tdd/Dollar.java
+++
@@ -1,8 +0,0 @@
-package com.challenge.tdd;
-
-public class Dollar extends Money {
-
-    Dollar(int amount, String currency) {
-        super(amount, currency);
-    }
-}
--- challenge/211209/src/main/java/com/challenge/tdd/Franc.java
+++
@@ -1,8 +0,0 @@
-package com.challenge.tdd;
-
-public class Franc extends Money {
-
-    Franc(int amount, String currency) {
-        super(amount, currency);
-    }
-}

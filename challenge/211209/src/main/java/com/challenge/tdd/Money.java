e8f247394676cfbde2c66ba71f4a7db15b2494c8
currenjin <hyun0524e@naver.com>
2022-01-01 00:37:29 +0900

    chapter-15

--- challenge/211209/src/main/java/com/challenge/tdd/Money.java
+++ challenge/211209/src/main/java/com/challenge/tdd/Money.java
@@ -23,7 +23,7 @@ public class Money implements Expression {
         return amount == money.amount && currency.equals(money.currency);
     }
 
-    public Money times(int multiplier) {
+    public Expression times(int multiplier) {
         return new Money(amount * multiplier, currency);
     }
 
@@ -35,8 +35,8 @@ public class Money implements Expression {
         return currency;
     }
 
-    public Expression plus(Money addend) {
-        return new Money(amount + addend.amount, currency);
+    public Expression plus(Expression addend) {
+        return new Sum(this, addend);
     }
 
     public Money reduce(Bank bank, String to) {
--- challenge/211209/src/test/java/com/challenge/tdd/MoneyTest.java
+++ challenge/211209/src/test/java/com/challenge/tdd/MoneyTest.java
@@ -65,4 +65,16 @@ class MoneyTest {
     void testIdentityRate() {
         assertThat(1).isEqualTo(new Bank().rate("USD", "USD"));
     }
+
+    @Test
+    void testMixedAddition() {
+        Expression fiveBucks = Money.dollar(5);
+        Expression tenFrancs = Money.franc(10);
+        Bank bank = new Bank();
+        bank.addRate("CHF", "USD", 2);
+
+        Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
+
+        assertThat(Money.dollar(10)).isEqualTo(result);
+    }
 }

2dca54e8f75f7119e8c861cad8041a7cc449393f
nowjin <hyun0524e@naver.com>
2022-03-17 22:31:34 +0900

    Validate logic 추가

--- java-baseball-playground/src/test/java/baseball/ValidateTest.java
+++ java-baseball-playground/src/test/java/baseball/ValidateTest.java
@@ -1,5 +1,6 @@
 package baseball;
 
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -12,5 +13,20 @@ public class ValidateTest {
         assertThat(Validator.validNo(9)).isTrue();
         assertThat(Validator.validNo(0)).isFalse();
         assertThat(Validator.validNo(10)).isFalse();
+    }
+
+    @Test
+    void 세자리_숫자인지_검증() {
+        assertThat(Validator.validSize(3, Lists.newArrayList(1, 2, 3))).isTrue();
+        assertThat(Validator.validSize(3, Lists.newArrayList(1, 2))).isFalse();
+        assertThat(Validator.validSize(3, Lists.newArrayList(1))).isFalse();
+        assertThat(Validator.validSize(3, Lists.newArrayList(1, 2, 3, 4))).isFalse();
+    }
+
+    @Test
+    void 중복_검증() {
+        assertThat(Validator.validNotDuplicate(Lists.newArrayList(1, 2, 3, 4, 5))).isTrue();
+        assertThat(Validator.validNotDuplicate(Lists.newArrayList(1, 2, 2))).isFalse();
+        assertThat(Validator.validNotDuplicate(Lists.newArrayList(1))).isTrue();
     }
 }

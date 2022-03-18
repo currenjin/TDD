803a03df7a983ac24ec1259e97a68c050f731e0a
nowjin <hyun0524e@naver.com>
2022-03-19 02:29:09 +0900

    Application 내 예외케이스 추가

--- challenge/220318/src/main/java/baseball/application/BaseballApplication.java
+++ challenge/220318/src/main/java/baseball/application/BaseballApplication.java
@@ -4,8 +4,11 @@ import baseball.domain.Balls;
 import baseball.domain.PlayResult;
 import baseball.util.NumberConverter;
 import baseball.util.RandomNumber;
+import baseball.util.Validator;
 
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class BaseballApplication {
     public static void main(String[] args) {
@@ -14,14 +17,39 @@ public class BaseballApplication {
 
         while (start) {
             System.out.print("숫자를 입력하세요 : ");
-            Scanner scanner = new Scanner(System.in);
+            List<Integer> numbers = NumberConverter.convert(new Scanner(System.in).nextLine());
 
-            PlayResult playResult = balls.play(NumberConverter.convert(scanner.nextLine()));
+            if (!validate(numbers)) continue;
+
+            PlayResult playResult = balls.play(numbers);
 
             System.out.println(String.format("%s 스트라이크 %s 볼", playResult.getStrike(), playResult.getBall()));
             start = isStart(playResult);
             balls = initBalls(playResult, balls);
         }
+    }
+
+    private static Boolean validate(List<Integer> numbers) {
+        Validator validator = new Validator();
+
+        if (!validator.validSize(3, numbers)) {
+            System.out.println("세 자리의 수를 입력해 주세요. 현재 입력 개수 : " + numbers.size());
+            return false;
+        }
+
+        if (!validator.isNotDuplicateNumber(numbers)) {
+            System.out.println("중복되는 수가 있습니다.");
+            return false;
+        }
+
+        Stream<Integer> filteredNumbers = numbers.stream().filter(n -> !validator.validNumber(n));
+
+        if (filteredNumbers.findFirst().isPresent()) {
+            System.out.println("1 ~ 9 사이의 수를 입력해 주세요.");
+            return false;
+        }
+
+        return true;
     }
 
     private static Balls initBalls(PlayResult playResult, Balls balls) {

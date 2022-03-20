be88a2909a53b021e994851b29f4b921a556a8bd
currenjin <hyun0524e@naver.com>
2022-03-20 13:12:42 +0900

    isSatisfy 에 따라 SG 생성 테스트 추가

--- specification/src/main/java/com/tdd/domain/ScheduleTriggerRuleMatchCondition.java
+++ specification/src/main/java/com/tdd/domain/ScheduleTriggerRuleMatchCondition.java
@@ -1,4 +1,7 @@
 package com.tdd.domain;
 
 public class ScheduleTriggerRuleMatchCondition {
+    public Object isSatisfy(Object object) {
+        return null;
+    }
 }
--- specification/src/main/java/com/tdd/domain/ScheduleTriggerRuleMatchPolicy.java
+++ specification/src/main/java/com/tdd/domain/ScheduleTriggerRuleMatchPolicy.java
@@ -1,4 +1,13 @@
 package com.tdd.domain;
 
+import java.util.Optional;
+
 public class ScheduleTriggerRuleMatchPolicy {
+    public ScheduleTriggerRuleMatchPolicy(ScheduleTriggerRuleMatchCondition condition, ScheduleTriggerRule rule) {
+
+    }
+
+    public Optional<ScheduleGroup> createScheduleGroup(Object object) {
+        return null;
+    }
 }

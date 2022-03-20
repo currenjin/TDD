e58391284974a61bdcfcf9b0235d5af792be7e01
nowjin <hyun0524e@naver.com>
2022-03-20 20:57:15 +0900

    initial commit

---
+++ challenge/220320/src/main/java/com/tdd/specification/Specification.java
@@ -0,0 +1,4 @@
+package com.tdd.specification;
+
+public class Specification {
+}
---
+++ challenge/220320/src/test/java/com/tdd/specification/SpecificationTest.java
@@ -0,0 +1,10 @@
+package com.tdd.specification;
+
+import org.junit.jupiter.api.Test;
+
+public class SpecificationTest {
+    @Test
+    void name() {
+        Specification specification = new Specification();
+    }
+}

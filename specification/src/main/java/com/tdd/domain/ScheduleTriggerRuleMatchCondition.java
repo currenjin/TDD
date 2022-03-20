1aceb0c49feee2bba0da7ddc42c2d99f88392e18
currenjin <hyun0524e@naver.com>
2022-03-20 13:55:30 +0900

    expected 를 통한 satisfify

--- specification/src/main/java/com/tdd/domain/ScheduleTriggerRuleMatchCondition.java
+++ specification/src/main/java/com/tdd/domain/ScheduleTriggerRuleMatchCondition.java
@@ -3,10 +3,22 @@ package com.tdd.domain;
 import java.util.Map;
 
 public class ScheduleTriggerRuleMatchCondition {
+    public static ScheduleTriggerRuleMatchCondition expected(String expectedKey, String expectedValue) {
+        return new ScheduleTriggerRuleMatchCondition(expectedKey, expectedValue);
+    }
+
+    private final String expectedKey;
+    private final String expectedValue;
+
+    public ScheduleTriggerRuleMatchCondition(String expectedKey, String expectedValue) {
+        this.expectedKey = expectedKey;
+        this.expectedValue = expectedValue;
+    }
+
     public boolean isSatisfy(Object object) {
         if (object instanceof Map) {
             Map<String, String> map = (Map<String, String>) object;
-            return map.containsKey("foo") || map.containsKey("bar");
+            return expectedValue.equals(map.get(expectedKey));
         }
 
         return false;
--- specification/src/test/java/com/tdd/domain/ScheduleTriggerRuleMatchConditionTest.java
+++ specification/src/test/java/com/tdd/domain/ScheduleTriggerRuleMatchConditionTest.java
@@ -4,18 +4,35 @@ import org.assertj.core.util.Maps;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Map;
+
+import static com.tdd.domain.ScheduleTriggerRuleMatchCondition.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ScheduleTriggerRuleMatchConditionTest {
 
     @Test
-    @DisplayName("is satisfied because contain foo")
+    @DisplayName("is satisfied because contain key and value")
     void map_contain() {
-        Object object1 = Maps.newHashMap("foo", "value");
-        Object object2 = Maps.newHashMap("bar", "value");
-        ScheduleTriggerRuleMatchCondition sut = new ScheduleTriggerRuleMatchCondition();
+        Map<String, String> map = Maps.newHashMap("jar", "foo");
 
-        assertThat(sut.isSatisfy(object1)).isTrue();
-        assertThat(sut.isSatisfy(object2)).isTrue();
+        assertThat(expected("jar", "xxxxx").isSatisfy(map)).isFalse();
+        assertThat(expected("jar", "foo").isSatisfy(map)).isTrue();
+    }
+
+    public static class ABC {
+        public boolean isSatisfy(Map<String, Object> factor) {
+            return expected("name", "foo").isSatisfy(factor)
+                    && expected("age", "18").isSatisfy(factor);
+        }
+    }
+
+    @Test
+    @DisplayName("is satisfied for multiple condition")
+    void multiple_condition() {
+        Map<String, Object> map = Maps.newHashMap("name", "foo");
+        map.put("age", "18");
+
+        assertThat(new ABC().isSatisfy(map)).isTrue();
     }
 }

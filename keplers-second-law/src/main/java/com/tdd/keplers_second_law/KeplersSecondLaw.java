dca4aec397b8ffc294a980e5592bb01fbd1d3fa5
Hyunjin Jeong <hyun0524e@naver.com>
2022-06-02 14:08:27 +0900

    add: package - keplers second law (#42)
    * add: package - keplers second law
    
    * update: README.md(for keplers first law)

---
+++ keplers-second-law/src/main/java/com/tdd/keplers_second_law/KeplersSecondLaw.java
@@ -0,0 +1,4 @@
+package com.tdd.keplers_second_law;
+
+public class KeplersSecondLaw {
+}
---
+++ keplers-second-law/src/main/java/com/tdd/keplers_second_law/KeplersSecondLawApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.keplers_second_law;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeplersSecondLawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeplersSecondLawApplication.class, args);
+	}
+
+}

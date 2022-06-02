80036720f5f58a74337bbe45ca1f9d900a0d7025
Hyunjin Jeong <hyun0524e@naver.com>
2022-06-02 14:01:47 +0900

    add: package - keplers first law (#41)
    * add: package - keplers first law
    
    * update: README.md(for keplers first law)

---
+++ keplers-first-law/src/main/java/com/tdd/keplers_first_law/KeplersFirstLawApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.keplers_first_law;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeplersFirstLawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeplersFirstLawApplication.class, args);
+	}
+
+}

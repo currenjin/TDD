b17b2d9d0442fe819db914328a67325379bd70a4
currenjin <hyun0524e@naver.com>
2022-07-16 19:52:58 +0900

    add: 220716 challenge fibonacci

---
+++ challenge/220716/src/main/java/com/tdd/fibonacci/Fibonacci.java
@@ -0,0 +1,9 @@
+package com.tdd.fibonacci;
+
+public class Fibonacci {
+    public static int fib(int n) {
+        if (n == 0) return 0;
+        if (n <= 2) return 1;
+        return fib(n - 1) + fib(n - 2);
+    }
+}

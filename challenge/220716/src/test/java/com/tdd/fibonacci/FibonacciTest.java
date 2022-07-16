4a2b0cb9cf41816818d4db303b33c414862c3730
Hyunjin-Jeong <hyun0524e@naver.com>
2022-07-16 19:53:59 +0900

    add: 220716 challenge fibonacci (#44)

---
+++ challenge/220716/src/test/java/com/tdd/fibonacci/FibonacciTest.java
@@ -0,0 +1,16 @@
+package com.tdd.fibonacci;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FibonacciTest {
+
+    @ParameterizedTest
+    @CsvSource(value = {"0, 0", "1, 1", "2, 1", "3, 2", "4, 3", "5, 5", "6, 8", "7, 13"})
+    void fibonacci(int n, int result) {
+        assertThat(Fibonacci.fib(n)).isEqualTo(result);
+    }
+}

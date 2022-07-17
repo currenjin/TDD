dc7d55ed18fc7af79884cf7af2cbe38eab3cc8b4
nowjin <hyun0524e@naver.com>
2022-07-17 17:56:05 +0900

    add: Planet orbit & Earth's orbit

---
+++ planetary-orbital-calculator/src/test/java/com/tdd/PlanetOrbitTest.java
@@ -0,0 +1,24 @@
+package com.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class PlanetOrbitTest {
+
+    @Test
+    void 지구_궤도를_생성합니다() {
+        PlanetOrbit actual = PlanetOrbit.of(EARTH.LONG_RADIUS,
+                EARTH.ECCENTRICITY,
+                EARTH.INCLINATION,
+                EARTH.LONGITUDE_OF_ASCENDING_NODE,
+                EARTH.AVERAGE_LONGITUDE,
+                EARTH.PERIHELION_LONGITUDE,
+                EARTH.AU,
+                EARTH.CHANGE_PER_CENTURY);
+
+        assertThat(actual).isInstanceOf(Orbit.class);
+        assertThat(actual.getAu()).isEqualTo(EARTH.AU);
+        assertThat(actual.getChangePerCentury()).isEqualTo(EARTH.CHANGE_PER_CENTURY);
+    }
+}

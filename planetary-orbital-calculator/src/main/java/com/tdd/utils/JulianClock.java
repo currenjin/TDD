952a234f4a5c283bcfa3206fff9db508f35a411d
currenjin <hyun0524e@naver.com>
2022-07-19 09:21:58 +0900

    add: JulianClock

---
+++ planetary-orbital-calculator/src/main/java/com/tdd/utils/JulianClock.java
@@ -0,0 +1,18 @@
+package com.tdd.utils;
+
+import static java.time.ZoneOffset.UTC;
+
+public class JulianClock {
+
+    public long elapsedDate() {
+        return getEpochTime() / TimeConstant.DAY;
+    }
+
+    public double elapsedCentury() {
+        return elapsedDate() / TimeConstant.CENTURY;
+    }
+
+    private long getEpochTime() {
+        return Clocks.now().toEpochSecond(UTC) - TimeConstant.J2000.toEpochSecond(UTC);
+    }
+}
---
+++ planetary-orbital-calculator/src/test/java/com/tdd/utils/JulianClockTest.java
@@ -0,0 +1,61 @@
+package com.tdd.utils;
+
+import org.junit.jupiter.api.Test;
+
+import static com.tdd.utils.TimeConstant.J2000;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class JulianClockTest {
+
+    JulianClock sut = new JulianClock();
+
+    @Test
+    void 역기점으로부터_하루가_지났다() {
+        TimeFreezer.freeze(J2000.plusDays(1));
+
+        assertThat(sut.elapsedDate()).isEqualTo(1);
+    }
+
+    @Test
+    void 역기점으로부터_하루_전이다() {
+        TimeFreezer.freeze(J2000.minusDays(1));
+
+        assertThat(sut.elapsedDate()).isEqualTo(-1);
+    }
+
+    @Test
+    void 역기점_당일이다() {
+        TimeFreezer.freeze(J2000);
+
+        assertThat(sut.elapsedDate()).isEqualTo(0);
+    }
+
+    @Test
+    void 역기점으로부터_1세기가_지났다() {
+        TimeFreezer.freeze(J2000.plusYears(100));
+
+        assertThat(sut.elapsedCentury()).isBetween(0.999, 1.0);
+    }
+
+    @Test
+    void 역기점으로부터_1세기_전이다() {
+        TimeFreezer.freeze(J2000.minusYears(100));
+
+        assertThat(sut.elapsedCentury()).isBetween(-1.0, -0.999);
+    }
+
+    @Test
+    void 역기점으로부터_10세기_전이다() {
+        TimeFreezer.freeze(J2000.minusYears(1000));
+
+
+        assertThat(sut.elapsedCentury()).isBetween(-10.0, -9.999);
+    }
+
+    @Test
+    void 역기점으로부터_20세기_후이다() {
+        TimeFreezer.freeze(J2000.plusYears(2000));
+
+        assertThat(sut.elapsedCentury()).isBetween(19.999, 20.0);
+    }
+}

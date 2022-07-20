30002fcc921e5618f595443fb46901b096326e57
currenjin <hyun0524e@naver.com>
2022-07-20 18:58:04 +0900

    add: true anomaly calculator

--- planetary-orbital-calculator/src/main/java/com/tdd/TrueAnomalyCalculator.java
+++ planetary-orbital-calculator/src/main/java/com/tdd/TrueAnomalyCalculator.java
@@ -2,6 +2,15 @@ package com.tdd;
 
 public class TrueAnomalyCalculator {
 
+    public static double calculate(Double eccentricity, Double averageLongitude, Double perihelionLongitude) {
+        validate(eccentricity, averageLongitude, perihelionLongitude);
+
+        double meanAnomaly = calculateMeanAnomaly(averageLongitude, perihelionLongitude);
+        double radianMeanAnomaly = meanAnomaly * (Math.PI / 180);
+
+        return (radianMeanAnomaly + (eccentricity * Math.sin(radianMeanAnomaly))) / (1 - (eccentricity * Math.cos(radianMeanAnomaly)));
+    }
+
     public static double calculateMeanAnomaly(Double averageLongitude, Double perihelionLongitude) {
         validateMeanAnomalyCalculator(averageLongitude, perihelionLongitude);
 
@@ -9,6 +18,20 @@ public class TrueAnomalyCalculator {
     }
 
     private static void validateMeanAnomalyCalculator(Double averageLongitude, Double perihelionLongitude) {
+        if (averageLongitude == null) {
+            throw new IllegalArgumentException();
+        }
+
+        if (perihelionLongitude == null) {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private static void validate(Double eccentricity, Double averageLongitude, Double perihelionLongitude) {
+        if (eccentricity == null) {
+            throw new IllegalArgumentException();
+        }
+
         if (averageLongitude == null) {
             throw new IllegalArgumentException();
         }
--- planetary-orbital-calculator/src/test/java/com/tdd/TrueAnomalyCalculatorTest.java
+++ planetary-orbital-calculator/src/test/java/com/tdd/TrueAnomalyCalculatorTest.java
@@ -1,5 +1,7 @@
 package com.tdd;
 
+import com.tdd.utils.TimeFreezer;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -7,27 +9,61 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class TrueAnomalyCalculatorTest {
 
-    public static final Double AVERAGE_LONGITUDE = 100.46457166;
-    public static final Double PERIHELION_LONGITUDE = 102.93768193;
+    public static final Double EPOCH_ECCENTRICITY = 0.01671123;
+    public static final Double EPOCH_AVERAGE_LONGITUDE = 100.46457166;
+    public static final Double EPOCH_PERIHELION_LONGITUDE = 102.93768193;
+
+    @AfterEach
+    void tearDown() {
+        TimeFreezer.rollback();
+    }
+
+    @Test
+    void 진근점이각을_계산한다() {
+        double actual = TrueAnomalyCalculator.calculate(EPOCH_ECCENTRICITY, EPOCH_AVERAGE_LONGITUDE, EPOCH_PERIHELION_LONGITUDE);
+
+        assertThat(actual).isEqualTo(-0.044630145101967715);
+    }
+
+    @Test
+    void 진근점이각을_계산시_이심률이_유효하지_않으면_안된다() {
+        assertThatThrownBy(() ->
+                TrueAnomalyCalculator.calculate(null, EPOCH_AVERAGE_LONGITUDE, EPOCH_PERIHELION_LONGITUDE))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 진근점이각을_계산시_평균_적경이_유효하지_않으면_안된다() {
+        assertThatThrownBy(() ->
+                TrueAnomalyCalculator.calculate(EPOCH_ECCENTRICITY, null, EPOCH_PERIHELION_LONGITUDE))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 진근점이각을_계산시_근일점_적경이_유효하지_않으면_안된다() {
+        assertThatThrownBy(() ->
+                TrueAnomalyCalculator.calculate(EPOCH_ECCENTRICITY, EPOCH_AVERAGE_LONGITUDE, null))
+                .isInstanceOf(IllegalArgumentException.class);
+    }
 
     @Test
     void 평균근점이각은_평균_경도에서_근일점_경도를_뺀_값이다() {
-        double actual = TrueAnomalyCalculator.calculateMeanAnomaly(AVERAGE_LONGITUDE, PERIHELION_LONGITUDE);
+        double actual = TrueAnomalyCalculator.calculateMeanAnomaly(EPOCH_AVERAGE_LONGITUDE, EPOCH_PERIHELION_LONGITUDE);
 
-        assertThat(actual).isEqualTo(AVERAGE_LONGITUDE - PERIHELION_LONGITUDE);
+        assertThat(actual).isEqualTo(EPOCH_AVERAGE_LONGITUDE - EPOCH_PERIHELION_LONGITUDE);
     }
 
     @Test
     void 평균근점이각_계산시_평균_경도가_유효하지_않으면_안된다() {
         assertThatThrownBy(() ->
-                TrueAnomalyCalculator.calculateMeanAnomaly(null, PERIHELION_LONGITUDE))
+                TrueAnomalyCalculator.calculateMeanAnomaly(null, EPOCH_PERIHELION_LONGITUDE))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 평균근점이각_계산시_근일점_경도가_유효하지_않으면_안된다() {
         assertThatThrownBy(() ->
-                TrueAnomalyCalculator.calculateMeanAnomaly(AVERAGE_LONGITUDE, null))
+                TrueAnomalyCalculator.calculateMeanAnomaly(EPOCH_AVERAGE_LONGITUDE, null))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 }

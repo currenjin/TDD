8843181d9d6754b8f8fb8345567112589b1f4c28
currenjin <hyun0524e@naver.com>
2024-07-20 22:01:22 +0900

    Feat: FileNode

---
+++ visitor/src/main/java/com/tdd/domain/filesystem/FileNode.java
@@ -0,0 +1,23 @@
+package com.tdd.domain.filesystem;
+
+public class FileNode {
+    private final int size;
+    private final String name;
+
+    public FileNode(String name, int size) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be null or empty");
+        }
+
+        this.name = name;
+        this.size = size;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+}
---
+++ visitor/src/test/java/com/tdd/domain/filesystem/FileNodeTest.java
@@ -0,0 +1,36 @@
+package com.tdd.domain.filesystem;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FileNodeTest {
+    @Test
+    void file_node_size() {
+        FileNode first_node = new FileNode("first_file", 5);
+        FileNode second_node = new FileNode("second_file", 10);
+
+        assertEquals(first_node.getSize(), 5);
+        assertEquals(second_node.getSize(), 10);
+    }
+
+    @Test
+    void file_node_name() {
+        FileNode first_node = new FileNode("first_file", 5);
+        FileNode second_node = new FileNode("second_file", 10);
+
+        assertEquals(first_node.getName(), "first_file");
+        assertEquals(second_node.getName(), "second_file");
+    }
+
+    @Test
+    void name_cannot_be_null() {
+        assertThrows(IllegalArgumentException.class, () -> new FileNode(null, 5));
+    }
+
+    @Test
+    void name_cannot_be_empty() {
+        assertThrows(IllegalArgumentException.class, () -> new FileNode("", 5));
+    }
+}

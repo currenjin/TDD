23ad54751388369a9bd7686bdecfde3b5efdcf10
currenjin <hyun0524e@naver.com>
2024-07-21 20:42:40 +0900

    Test: Only Directory Visitor Test

--- visitor/src/test/java/com/tdd/domain/filesystem/FileSizeVisitorTest.java
+++ visitor/src/test/java/com/tdd/domain/filesystem/FileSizeVisitorTest.java
@@ -33,6 +33,28 @@ public class FileSizeVisitorTest {
     }
 
     @Test
+    void only_one_directory() {
+        DirectoryNode directory = new DirectoryNode("directory");
+
+        int totalSize = directory.accept(visitor);
+
+        assertEquals(0, totalSize);
+    }
+
+    @Test
+    void only_directory() {
+        DirectoryNode firstDirectory = new DirectoryNode("directory");
+        DirectoryNode secondDirectory = new DirectoryNode("directory");
+        DirectoryNode integrationDirectory = new DirectoryNode("directory");
+        integrationDirectory.addChildren(firstDirectory);
+        integrationDirectory.addChildren(secondDirectory);
+
+        int totalSize = integrationDirectory.accept(visitor);
+
+        assertEquals(0, totalSize);
+    }
+
+    @Test
     void two_file_in_both_directory_in_directory() {
         FileNode firstFile = new FileNode("file", 1);
         FileNode secondFile = new FileNode("file", 2);

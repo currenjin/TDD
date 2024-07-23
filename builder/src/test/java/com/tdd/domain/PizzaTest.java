7af32514ce9ce0783089213021508bcbe7508ef6
currenjin <hyun0524e@naver.com>
2024-07-23 13:11:51 +0900

    Test: Refactor extract constant size

--- builder/src/test/java/com/tdd/domain/PizzaTest.java
+++ builder/src/test/java/com/tdd/domain/PizzaTest.java
@@ -5,36 +5,40 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PizzaTest {
+
+    public static final int SMALL_SIZE = 4;
+    public static final int LARGE_SIZE = 8;
+
     @Test
     void pizza() {
-        Pizza smallPizza = new Pizza(4);
-        Pizza largePizza = new Pizza(8);
+        Pizza smallPizza = new Pizza(SMALL_SIZE);
+        Pizza largePizza = new Pizza(LARGE_SIZE);
 
-        assertEquals(4, smallPizza.getSize());
-        assertEquals(8, largePizza.getSize());
+        assertEquals(SMALL_SIZE, smallPizza.getSize());
+        assertEquals(LARGE_SIZE, largePizza.getSize());
     }
 
     @Test
     void pizza_with_cheese() {
-        Pizza pizza = new Pizza(4, true);
+        Pizza pizza = new Pizza(SMALL_SIZE, true);
         assertEquals(true, pizza.getCheese());
     }
 
     @Test
     void pizza_without_cheese() {
-        Pizza pizza = new Pizza(4, false);
+        Pizza pizza = new Pizza(SMALL_SIZE, false);
         assertEquals(false, pizza.getCheese());
     }
 
     @Test
     void pizza_with_peperoni() {
-        Pizza pizza = new Pizza(4, false, true);
+        Pizza pizza = new Pizza(SMALL_SIZE, false, true);
         assertEquals(true, pizza.getPeperoni());
     }
 
     @Test
     void pizza_without_peperoni() {
-        Pizza pizza = new Pizza(4, false, false);
+        Pizza pizza = new Pizza(SMALL_SIZE, false, false);
         assertEquals(false, pizza.getPeperoni());
     }
 }

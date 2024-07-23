4014260dbebcb8eb74cbc383b321322ffd0e5160
currenjin <hyun0524e@naver.com>
2024-07-23 21:04:59 +0900

    Feat: PizzaBuilder and Test

--- builder/src/main/java/com/tdd/domain/PizzaBuilder.java
+++ builder/src/main/java/com/tdd/domain/PizzaBuilder.java
@@ -1,14 +1,26 @@
 package com.tdd.domain;
 
 public class PizzaBuilder {
-    private PizzaSize size;
+    private PizzaSize size = PizzaSize.SMALL;
+    private boolean cheese = false;
+    private boolean peperoni = false;
 
     public PizzaBuilder size(PizzaSize size) {
         this.size = size;
         return this;
     }
 
+    public PizzaBuilder cheese() {
+        this.cheese = true;
+        return this;
+    }
+
+    public PizzaBuilder peperoni() {
+        this.peperoni = true;
+        return this;
+    }
+
     public Pizza build() {
-        return new Pizza(size);
+        return new Pizza(size, cheese, peperoni);
     }
 }
--- builder/src/test/java/com/tdd/domain/PizzaBuilderTest.java
+++ builder/src/test/java/com/tdd/domain/PizzaBuilderTest.java
@@ -2,15 +2,81 @@ package com.tdd.domain;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class PizzaBuilderTest {
+    PizzaBuilder builder = new PizzaBuilder();
+
     @Test
     void build_pizza_with_size() {
-        PizzaBuilder builder = new PizzaBuilder();
-
         Pizza pizza = builder.size(PizzaSize.SMALL).build();
 
         assertEquals(PizzaSize.SMALL, pizza.getSize());
+        assertFalse(pizza.getCheese());
+        assertFalse(pizza.getPeperoni());
+    }
+
+    @Test
+    void build_pizza_with_cheese() {
+        Pizza pizza = builder.cheese().build();
+
+        assertTrue(pizza.getCheese());
+        assertEquals(PizzaSize.SMALL, pizza.getSize());
+        assertFalse(pizza.getPeperoni());
+    }
+
+    @Test
+    void build_pizza_with_peperoni() {
+        Pizza pizza = builder.peperoni().build();
+
+        assertTrue(pizza.getPeperoni());
+        assertEquals(PizzaSize.SMALL, pizza.getSize());
+        assertFalse(pizza.getCheese());
+    }
+
+    @Test
+    void build_pizza_with_size_and_cheese() {
+        Pizza pizza = builder
+                .size(PizzaSize.LARGE)
+                .cheese()
+                .build();
+
+        assertEquals(PizzaSize.LARGE, pizza.getSize());
+        assertTrue(pizza.getCheese());
+        assertFalse(pizza.getPeperoni());
+    }
+
+    @Test
+    void build_pizza_with_size_and_cheese_and_peperoni() {
+        Pizza pizza = builder
+                .size(PizzaSize.LARGE)
+                .cheese()
+                .peperoni()
+                .build();
+
+        assertEquals(PizzaSize.LARGE, pizza.getSize());
+        assertTrue(pizza.getCheese());
+        assertTrue(pizza.getPeperoni());
+    }
+
+    @Test
+    void build_pizza_with_cheese_and_peperoni() {
+        Pizza pizza = builder
+                .cheese()
+                .peperoni()
+                .build();
+
+        assertEquals(PizzaSize.SMALL, pizza.getSize());
+        assertTrue(pizza.getCheese());
+        assertTrue(pizza.getPeperoni());
+    }
+
+    @Test
+    void build_pizza() {
+        Pizza pizza = builder.build();
+
+        assertEquals(PizzaSize.SMALL, pizza.getSize());
+        assertFalse(pizza.getCheese());
+        assertFalse(pizza.getPeperoni());
     }
 }

5f95378c6adeb6f52e908b640c5ea4ca20260535
currenjin <hyun0524e@naver.com>
2024-09-02 07:25:15 +0900

    Feat: FileNode name cannot be null or empty or blank

--- challenge/240902/src/main/java/com/tdd/domain/filesystem/FileNode.java
+++ challenge/240902/src/main/java/com/tdd/domain/filesystem/FileNode.java
@@ -5,8 +5,16 @@ public class FileNode {
     private final String name;
 
     public FileNode(String name, int size) {
+        validate(name);
+
         this.name = name;
         this.size = size;
+    }
+
+    private void validate(String name) {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot be null");
+        }
     }
 
     public int getSize() {
--- challenge/240902/src/test/java/com/tdd/domain/filesystem/FileNodeTest.java
+++ challenge/240902/src/test/java/com/tdd/domain/filesystem/FileNodeTest.java
@@ -3,6 +3,7 @@ package com.tdd.domain.filesystem;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class FileNodeTest {
     @Test
@@ -21,5 +22,13 @@ public class FileNodeTest {
 
         assertEquals(firstNode.getName(), "first");
         assertEquals(secondNode.getName(), "second");
+    }
+
+    @Test
+    void name_cannot_be_null_or_empty_or_blank() {
+        assertThrows(IllegalArgumentException.class, () -> new FileNode(null, 5));
+        assertThrows(IllegalArgumentException.class, () -> new FileNode("", 5));
+        assertThrows(IllegalArgumentException.class, () -> new FileNode(" ", 5));
+        assertThrows(IllegalArgumentException.class, () -> new FileNode("        ", 5));
     }
 }

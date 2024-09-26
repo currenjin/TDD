4b4964f2513e3df049636f96460fbdb5758292fb
currenjin <hyun0524e@naver.com>
2024-09-26 13:17:59 +0900

    Init: Kepler Third Law

---
+++ kepler-laws/src/main/java/com/tdd/util/KeplerThirdLaw.java
@@ -0,0 +1,7 @@
+package com.tdd.util;
+
+public class KeplerThirdLaw {
+    public static double calculateOrbitalPeriod(double semiMajorAxis, double period) {
+        return 0.0;
+    }
+}
---
+++ kepler-laws/src/test/java/com/tdd/util/KeplerThirdLawTest.java
@@ -0,0 +1,20 @@
+package com.tdd.util;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class KeplerThirdLawTest {
+
+    public static final double DELTA = 0.01;
+
+    @Test
+    void testOrbitalPeriodEarth() {
+        double semiMajorAxis = 1.0;
+        double expectedPeriod = 1.0;
+
+        double actualPeriod = KeplerThirdLaw.calculateOrbitalPeriod(semiMajorAxis, expectedPeriod);
+
+        assertEquals(expectedPeriod, actualPeriod, DELTA);
+    }
+}

95715629a4ad450a1a9c30bcd07efb81d40ec857
currenjin <hyun0524e@naver.com>
2024-10-03 11:20:55 +0900

    Test: All the planet of solar system

--- kepler-laws/src/test/java/com/tdd/util/KeplerThirdLawTest.java
+++ kepler-laws/src/test/java/com/tdd/util/KeplerThirdLawTest.java
@@ -1,12 +1,33 @@
 package com.tdd.util;
 
-import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
 
 class KeplerThirdLawTest {
 
     public static final double DELTA = 0.01;
+    public static final double CENTRAL_MASS = 1.0;
+
+    @Test
+    public void testCalculateOrbitalPeriodMercury() {
+        double semiMajorAxis = 0.39;
+        double expectedPeriod = 0.24;
+
+        double actualPeriod = KeplerThirdLaw.calculateOrbitalPeriod(semiMajorAxis, CENTRAL_MASS);
+
+        assertEquals(expectedPeriod, actualPeriod, DELTA);
+    }
+
+    @Test
+    public void testCalculateOrbitalPeriodVenus() {
+        double semiMajorAxis = 0.72;
+        double expectedPeriod = 0.62;
+
+        double actualPeriod = KeplerThirdLaw.calculateOrbitalPeriod(semiMajorAxis, CENTRAL_MASS);
+
+        assertEquals(expectedPeriod, actualPeriod, DELTA);
+    }
 
     @Test
     void testOrbitalPeriodEarth() {
@@ -21,10 +42,29 @@ class KeplerThirdLawTest {
     @Test
     public void testCalculateOrbitalPeriodMars() {
         double semiMajorAxis = 1.52;
-        double centralMass = 1.0;
         double expectedPeriod = 1.88;
 
-        double actualPeriod = KeplerThirdLaw.calculateOrbitalPeriod(semiMajorAxis, centralMass);
+        double actualPeriod = KeplerThirdLaw.calculateOrbitalPeriod(semiMajorAxis, CENTRAL_MASS);
+
+        assertEquals(expectedPeriod, actualPeriod, DELTA);
+    }
+
+    @Test
+    public void testCalculateOrbitalPeriodJupiter() {
+        double semiMajorAxis = 5.20;
+        double expectedPeriod = 11.86;
+
+        double actualPeriod = KeplerThirdLaw.calculateOrbitalPeriod(semiMajorAxis, CENTRAL_MASS);
+
+        assertEquals(expectedPeriod, actualPeriod, DELTA);
+    }
+
+    @Test
+    public void testCalculateOrbitalPeriodSaturn() {
+        double semiMajorAxis = 9.54;
+        double expectedPeriod = 29.46;
+
+        double actualPeriod = KeplerThirdLaw.calculateOrbitalPeriod(semiMajorAxis, CENTRAL_MASS);
 
         assertEquals(expectedPeriod, actualPeriod, DELTA);
     }
